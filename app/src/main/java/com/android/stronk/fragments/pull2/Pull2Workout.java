e770b7f1632f380f3d1d907f612d28cbe07a15a0
Simin <siminjacobs@gmail.com>
2015-09-29 08:16:10 +0200

    Fix layout push 1 and pull 2.

--- app/src/main/java/com/android/stronk/fragments/pull2/Pull2Workout.java
+++ app/src/main/java/com/android/stronk/fragments/pull2/Pull2Workout.java
@@ -31,7 +31,7 @@ public class Pull2Workout extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_push1_workout,
+        View view = inflater.inflate(R.layout.fragment_pull2_workout,
                 container, false);
 
         // Chronometer.
--- app/src/main/java/com/android/stronk/fragments/push1/Push1Workout.java
+++ app/src/main/java/com/android/stronk/fragments/push1/Push1Workout.java
@@ -31,7 +31,7 @@ public class Push1Workout extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_pull2_workout,
+        View view = inflater.inflate(R.layout.fragment_push1_workout,
                 container, false);
 
         // Chronometer.
